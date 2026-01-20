94739c22c2db82f272341bbf6fa13b3c1c88ec4d
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2026-01-20 23:52:29 +0100

    fix: Rename banksearch MLB extractor

--- src/constraints/extractor.py
+++ src/constraints/extractor.py
@@ -22,19 +22,24 @@ class BaseExtractor(ABC):
         """
         return [f"{c1},{c2},{parent}" for c1, c2 in combinations(children, 2)]
 
-class TreeBankExtractor(BaseExtractor):
+
+class BankSearchExtractor(BaseExtractor):
 
     def __init__(self):
-        super().__init__(dataset_name="treebank")
-        self.path2category_hierarchy = Path("resources/banksearch/category_hierarchy.json")
-        assert self.path2category_hierarchy.exists() and self.path2category_hierarchy.is_file(), \
-            f"Path to input categories is erroneous, check {self.path2category_hierarchy}."
+        super().__init__(dataset_name="banksearch")
+        self.path2category_hierarchy = Path(
+            "resources/banksearch/category_hierarchy.json"
+        )
+        assert (
+            self.path2category_hierarchy.exists()
+            and self.path2category_hierarchy.is_file()
+        ), f"Path to input categories is erroneous, check {self.path2category_hierarchy}."
 
     def extract_all_mlb_constraints(self, out_path: Path):
         """
-        Extract all MLB constraints from hierarchy dictionary. 
+        Extract all MLB constraints from hierarchy dictionary.
         Only works for flat hierarchies so far.
-        
+
         :param out_path: Path to save MLB constraints to (as txt file).
         """
         out_path.mkdir(parents=True, exist_ok=True)
@@ -46,7 +51,9 @@ class TreeBankExtractor(BaseExtractor):
 
         constraints = []
         for parent, children in hierarchy_dict.items():
-            constraints.extend(self._get_mlb_constraints(parent=parent, children=children))
+            constraints.extend(
+                self._get_mlb_constraints(parent=parent, children=children)
+            )
 
         logger.info(f"Saving {len(constraints)} constraints to {out_filename}")
         with open(out_filename, "w") as f:
@@ -55,15 +62,16 @@ class TreeBankExtractor(BaseExtractor):
 
         logger.info("Constraints successfully saved.")
 
+
 if __name__ == "__main__":
     # Configure logger
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    treebank_extractor = TreeBankExtractor()
+    banksearch_extractor = BankSearchExtractor()
     out_path = Path("resources/banksearch")
     out_path.mkdir(parents=True, exist_ok=True)
-    treebank_extractor.extract_all_mlb_constraints(out_path=out_path)
+    banksearch_extractor.extract_all_mlb_constraints(out_path=out_path)
