6eeb85affbe4b0dfebf607caf09e88855530b998
kgutekunst <klara.gutekunst@uni-kassel.de>
2026-01-24 16:13:12 +0100

    task: Add potential issue note

--- src/experiments/run_clj_file.py
+++ src/experiments/run_clj_file.py
@@ -152,6 +152,8 @@ try:
         extractor = BankSearchTopicModelExtractor(iceberg_concepts=read_edn_concepts(edn_path))
         out_path = Path("resources/banksearch")
         out_path.mkdir(parents=True, exist_ok=True)
+        # FIXME: Due to one topic per document there is the uppermost concept with no topics, which leads to empty last constraint, which does not make sense
+        # I think, we NEED more than one topic per document to have meaningful constraints
         extractor.extract_all_mlb_constraints(out_path=out_path)
 
         print("Extracted iceberg concepts:", out_path)
