85cecb1fca6a4a2cf931338729d21a23a4ea0ebe
kgutekunst <klara.gutekunst@uni-kassel.de>
2026-01-28 14:22:28 +0100

    fix: Extract more than one topic per document

--- src/topic_model/dataset2lda_topics.py
+++ src/topic_model/dataset2lda_topics.py
@@ -19,7 +19,9 @@ from gensim.models import LdaModel
 # Ground truth: total number of topics in BankSearch dataset is 11
 NUM_TOPICS_CORPUS = 11  # number of latent topics
 # since in ground truth each document has exactly one topic, we extract the same number of dominant topics
-N_TOPICS_PER_DOC = 1  # number of dominant topics to extract per document
+N_TOPICS_PER_DOC = 5  # number of dominant topics to extract per document
+assert N_TOPICS_PER_DOC <= NUM_TOPICS_CORPUS, "Cannot extract more topics per document than total topics in corpus."
+assert N_TOPICS_PER_DOC > 1, "Must extract at least one topic per document, otherwise later steps work on nominal scala which produces non-sense constraints."
 N_TOP_WORDS_PER_TOPIC = 10
 MIN_TOKEN_LENGTH = 3
 
